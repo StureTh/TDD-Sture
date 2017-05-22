1b326a2ff6ab856d8e97e09271d1bbbccfd90abe
StureTh <sturethuren@gmail.com>
2017-05-22 13:11:55 +0200

    Exercise1 Is done

---
+++ Exercise1/ValidationEngine/Validator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace ValidationEngine
+{
+   public class Validator
+    {
+        public bool ValidateEmailAddress(string email)
+        {
+             var regex = new Regex(@"^([\D]+)@([\D]+)((\.(\D){2,3})+)$");
+
+           
+           
+
+            return regex.IsMatch(email);
+        }
+        
+    }
+}
---
+++ Exercise1/ValidationEngineTests/ValidationTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValidationEngine;
+namespace ValidationEngineTests
+{
+    [TestFixture]
+   public class ValidationTests
+    {
+        
+        [Test]
+        public void TrueForValidAdress()
+        {
+            var sut = new Validator();
+
+            var valid = sut.ValidateEmailAddress("sture@mail.com");
+
+            Assert.IsTrue(valid);
+        }
+
+        [Test]
+        public void FalseForInvalidAdress()
+        {
+            var sut = new Validator();
+
+            var invalid = sut.ValidateEmailAddress("sture21huren@mail.com");
+
+            Assert.IsFalse(invalid);
+        }
+
+        [Test]
+
+        public void AdressIsNull()
+        {
+            var sut = new Validator();
+
+            var isNull = sut.ValidateEmailAddress("");
+
+            Assert.IsFalse(isNull);
+        }
+
+     
+    }
+}
